fb04ba4ad7951f85b0a5932fe2ffa420690997c8
reignolivia <olfrazer@packer.edu>
2026-07-14 15:31:10 -0400

    Incoporating story transitions
    Added intro images and story screen functionality.

--- 2247_testing.py
+++ 2247_testing.py
@@ -17,6 +17,25 @@ RED = (95,183,207)
 background = pygame.image.load("2247_earth.png")
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
+#intro images
+intro1 = pygame.image.load("Intro1.png")
+intro2 = pygame.image.load("Intro2.png")
+
+intro1 = pygame.transform.scale(intro1, (WIDTH, HEIGHT))
+intro2 = pygame.transform.scale(intro2, (WIDTH, HEIGHT))
+
+# Story screen variables
+story_images = [intro1, intro2]
+current_story = 0
+story_alpha = 0
+story_fade_speed = 4
+
+#rockets
+rocket1 = pygame.image.load("rocket1.png")
+rocket2 = pygame.image.load("rocket2.png")
+rocket1 = pygame.transform.scale(rocket1, (50, 100))
+rocket2 = pygame.transform.scale(rocket2, (50, 100))
+
 #player images
 player = pygame.image.load("astro_front.png")
 walk_right = pygame.image.load("astro_right.png")
@@ -25,6 +44,13 @@ walk_left = pygame.image.load("astro_left.png")
 idle = pygame.transform.scale(player, (160, 160))
 walk_right = pygame.transform.scale(walk_right, (160, 160))
 walk_left = pygame.transform.scale(walk_left, (160, 160))
+
+#rocket variables
+rocket1_x = 0
+rocket1_y = 0
+rocket2_x = 0
+rocket2_y = 0
+speed = 10
 
 #player variables
 player_x = 100
@@ -73,10 +99,24 @@ while running:
             if game_state == "menu":
 
                 if new_game_button.collidepoint(event.pos):
-                    game_state = "game"
+                    game_state = "story"
+                    current_story = 0
+                    story_alpha = 0
 
                 elif quit_button.collidepoint(event.pos):
                     running = False
+
+        if event.type == pygame.KEYDOWN:
+
+            if game_state == "story":
+
+                if event.key == pygame.K_RETURN:
+
+                    current_story += 1
+                    story_alpha = 0
+
+                    if current_story >= len(story_images):
+                        game_state = "game"
 
     # Draw Everything
     screen.blit(background, (0, 0))
@@ -111,9 +151,7 @@ while running:
                 fade_alpha = 0
                 game_state = "menu"
 
-    # =========================
-    # MENU
-    # =========================
+    #1. MENU
     elif game_state == "menu":
 
         menu_title = title_font.render("Save Earth: 2247", True, BLUE)
@@ -122,7 +160,7 @@ while running:
         # Mouse position
         mouse_pos = pygame.mouse.get_pos()
 
-# ---------- Start Game Button ----------
+    #Start Game Button
         if new_game_button.collidepoint(mouse_pos):
             start_button = new_game_button.inflate(20, 10)   # Makes button bigger
             start_color = GAME_BLUE                          # Hover color
@@ -136,7 +174,7 @@ while running:
         screen.blit(new_text, new_text.get_rect(center=start_button.center))
 
 
-# ---------- Quit Button ----------
+    #Quit Game Button
         if quit_button.collidepoint(mouse_pos):
             quit_button_draw = quit_button.inflate(20, 10)
             quit_color = GAME_BLUE
@@ -148,10 +186,19 @@ while running:
 
         quit_text = button_font.render("Quit", True, WHITE)
         screen.blit(quit_text, quit_text.get_rect(center=quit_button_draw.center))
-        
-    # =========================
-    # GAMEPLAY
-    # =========================
+
+    #2. INTRO STORYYY
+    elif game_state == "story":
+        story_image = story_images[current_story]
+
+        if story_alpha < 255:
+            story_alpha += story_fade_speed
+
+        story_image.set_alpha(story_alpha)
+
+        screen.blit(story_image, (0, 0))
+
+    #3. GAMEPLAY
     elif game_state == "game":
         keys = pygame.key.get_pressed()
 
